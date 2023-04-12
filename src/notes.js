21abdcc46f324bee23f2859ce59e594ec1912f97
Shubham Patel <shubhu2610@gmail.com>
2023-04-12 12:19:51 +0530

    notes commit

--- src/notes.js
+++ src/notes.js
@@ -1,23 +1,38 @@
 const notes = [{
         key: 1,
-        title: "Delegation",
-        content: "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+        title: "HTML",
+        content: "HTML is the markup language that we use to structure and give meaning to our web content, for example defining paragraphs, headings, and data tables, or embedding images and videos in the page."
     },
     {
         key: 2,
-        title: "Loops",
-        content: "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+        title: "CSS",
+        content: "CSS is a language of style rules that we use to apply styling to our HTML content, for example setting background colors and fonts, and laying out our content in multiple columns."
     },
     {
         key: 3,
-        title: "Arrays",
-        content: "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+        title: "JavaScript",
+        content: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else."
     },
     {
         key: 4,
-        title: "Hardware vs. Software",
-        content: "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
-    }
+        title: "ReactJS",
+        content: "React is a JavaScript-based UI development library. Facebook and an open-source developer community run it. Although React is a library rather than a language, it is widely used in web development."
+    },
+    {
+        key: 5,
+        title: "NodeJS",
+        content: "Node. js (Node) is an open source, cross-platform runtime environment for executing JavaScript code. Node is used extensively for server-side programming, making it possible for developers to use JavaScript."
+    },
+    {
+        key: 6,
+        title: "ExpressJS",
+        content: "Express is a node js web application framework that provides broad features for building web and mobile applications. It is used to build a single page, multipage, and hybrid web application."
+    },
+    {
+        key: 7,
+        title: "MongoDB",
+        content: "MongoDB is a non-relational document database that provides support for JSON-like storage. The MongoDB database has a flexible data model that enables you to store unstructured data, and it provides full indexing support, and replication with rich and intuitive APIs."
+    },
 ];
 
 export default notes;
